65842f4d7c1cb28dc3c731a3ff97a5d822af3803
Alexander Schulte <alex11.schulte@googlemail.com>
2021-03-22 15:21:03 +0100

    Added JavaScript and created a lib folder

--- profile.js
+++ profile.js
@@ -4,11 +4,16 @@ const footer = document.querySelector('.container__content__footer')
 const header = document.querySelector('.container__header')
 const cards = document.querySelectorAll('.container__content__profile')
 
-button.addEventListener('click', () => {
+button.addEventListener('click', event => {
   test.classList.toggle('darkmode')
   footer.classList.toggle('darkmode_footer_header')
   header.classList.toggle('darkmode_footer_header')
   for (i = 0; i < cards.length; i++) {
     cards[i].classList.toggle('darkmode_footer_header')
+    if (event.target.textContent === 'darkmode') {
+      event.target.textContent = 'default mode'
+    } else {
+      event.target.textContent = 'darkmode'
+    }
   }
 })
